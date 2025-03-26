726cac4a9776ac97b607518d96eafc3c0b208682
Alvaro Araoz <hola@appfilia.co>
2025-03-26 12:47:57 +0100

    Bugs fixed

--- next.config.mjs
+++ next.config.mjs
@@ -4,13 +4,13 @@ const nextConfig = {
   // Add rewrites to handle direct URL access
   async rewrites() {
     return [
-      // Redirect URLs like /https:/www.example.com to /proxy?url=https:/www.example.com
+      // Redirect URLs like /https:/www.example.com to /proxy?url=https://www.example.com
       {
-        source: '/https/:path*',
+        source: '/https:/:path*',
         destination: '/proxy?url=https://:path*',
       },
       {
-        source: '/http/:path*',
+        source: '/http:/:path*',
         destination: '/proxy?url=http://:path*',
       },
       // Handle URLs without protocol (e.g., /www.example.com)
